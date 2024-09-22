43d8a63092c8305cc51ac8ee079c2347fb6b394e
huyzx2911 <huyzx2911@gmail.com>
2024-09-23 01:31:11 +0700

    Update format terraform code

--- infra/network/subnet.tf
+++ infra/network/subnet.tf
@@ -1,8 +1,8 @@
 resource "google_compute_subnetwork" "workbench_subhet" {
-    name = format("%s-vertex-subnet", var.vpc_network_name)
-    network = google_compute_network.vpc_network.id
-    description = "Subnet for user workbench instance"
-    region = var.region
-    ip_cidr_range = var.subnet
-    private_ip_google_access = true
+  name                     = format("%s-vertex-subnet", var.vpc_network_name)
+  network                  = google_compute_network.vpc_network.id
+  description              = "Subnet for user workbench instance"
+  region                   = var.region
+  ip_cidr_range            = var.subnet
+  private_ip_google_access = true
 }

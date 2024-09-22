159211fe13032ffcab4fbc3ae2ca40b2623088b8
huyzx2911 <huyzx2911@gmail.com>
2024-09-23 06:11:11 +0700

    Update permission

--- scripts/generation_matrix.sh
+++ scripts/generation_matrix.sh
@@ -5,7 +5,6 @@ DIRECTORY="../workbench_instances/users_tfvars"  # Replace with your directory
 # Start the JSON object
 output='{"user_config": ['
 
-# Loop through the files and build the JSON array
 for file in "$DIRECTORY"/*; do
   filename=$(basename "$file")
   if [[ -n "$output" && "$output" != '{"user_config": [' ]]; then
